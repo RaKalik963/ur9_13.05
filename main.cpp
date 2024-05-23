1f553c237fd193a66e79a5f7ff43a503cedae04f
RaKalik963 <andrejmahot646@gmail.com>
2024-05-23 20:23:00 +0300

    lab-ex1

--- main.cpp
+++ main.cpp
@@ -3,42 +3,44 @@
 using namespace std;
 
 int main() {
-    srand(time(0));
-    const int FULL_SIZE = 15;
-    int arr[FULL_SIZE];
-    int size;
+	srand(time(0));
+	const int FULL_SIZE = 15;
+	int arr[FULL_SIZE];
+	int size;
+	int count = 0;
 
-    do {
-        cout << "Enter the size of the array: ";
-        cin >> size;
-    } while (size > FULL_SIZE || size <= 0);
+	cout << "Enter a size of array:";
+	do {
+		cin >> size;
+	} while (size > FULL_SIZE || size <= 0);
 
-    for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 200 - 100;
-    }
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 20 - 10;
+	}
 
-    cout << "Array: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+	cout << "Array before changes:" << endl;
 
-    int index = 0;
-    for (int i = 0; i < size; i++) {
-        if (arr[i] != 0) {
-            arr[index++] = arr[i];
-        }
-    }
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < size; i++) {
+		if (arr[i] != 0) {
+			arr[count++] = arr[i];
+		}
+	}
+	cout << endl;
+	cout << "Array after changes:" << endl;
 
-    while (index < FULL_SIZE) {
-        arr[index++] = -1;
-    }
+	for (int i = 0; i < size; i++) {
+		if (i >= count) {
+			arr[i] = -1;
+		}
+	}
 
-    cout << "Changed Array: ";
-    for (int i = 0; i < FULL_SIZE; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
 
-    return 0;
+	return 0;
 }
