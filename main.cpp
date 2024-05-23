3a87882ea08068b496ff407ed1bc11a2377950f2
RaKalik963 <andrejmahot646@gmail.com>
2024-05-23 20:15:03 +0300

    dz-ex2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,42 @@ using namespace std;
 
 int main()
 {
+    srand(time(0));
+    const int size = 10;
+    int arr1[size];
+    int arr2[size];
+    int result[size];
 
+    for (size_t i = 0; i < size; i++)
+    {
+        arr1[i] = rand() % 20 - 10;
+    }
+    
+    for (size_t i = 0; i < size; i++)
+    {
+        arr2[i] = rand() % 20 - 10;
+    }
+    cout << "Array1: ";
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr1[i] << " ";
+    }
+    cout << endl;
+    cout << "Array2: ";
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < size; ++i) {
+        result[i] = arr1[i] + arr2[i];
+    }
+
+    cout << "result ";
+    for (int i = 0; i < size; ++i) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
 	system("pause");
 	return 0;
 }
