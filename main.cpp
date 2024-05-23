cde52571155686b55ba12afe6f328406f1dc1b2d
RaKalik963 <andrejmahot646@gmail.com>
2024-05-23 19:55:07 +0300

    dz-ex1

--- main.cpp
+++ main.cpp
@@ -4,6 +4,32 @@ using namespace std;
 
 int main()
 {
+    const int SIZE1 = 10;
+    const int SIZE2 = 5;
+
+    int array1[SIZE1] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int array2[SIZE2];
+    int array3[SIZE2];
+
+    for (int i = 0; i < SIZE2; ++i) {
+        array2[i] = array1[i];
+    }
+
+    for (int i = 0; i < SIZE2; ++i) {
+        array3[i] = array1[i + SIZE2];
+    }
+
+    cout << "Array2: ";
+    for (int i = 0; i < SIZE2; ++i) {
+        cout << array2[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Array3: ";
+    for (int i = 0; i < SIZE2; ++i) {
+        cout << array3[i] << " ";
+    }
+    cout << endl;
 
 	system("pause");
 	return 0;
